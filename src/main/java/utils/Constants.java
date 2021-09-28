6fda4875bf5d9cab24489bc7eda51c31f5468355
WestJack777 <wesley.sales@live.com>
2021-09-28 12:06:40 -0300

    Envio do Constants

---
+++ src/main/java/utils/Constants.java
@@ -0,0 +1,11 @@
+package utils;
+
+public class Constants {
+	
+	//Use esta constante para definir o seu browser. Serao aceitos os valores FIREFOX e CHROME.
+	public static final String BROWSER_NAME="chrome";
+	
+	public static final String URL_HOME="http://advantageonlineshopping.com/#/";
+	public static final String URL_CADASTRO="http://advantageonlineshopping.com/#/register";
+	public static final String MSG_USUARIO_EXISTENTE="User name already exists";
+}
